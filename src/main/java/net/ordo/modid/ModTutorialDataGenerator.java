9a120cefc3c91ef6ff2684347b15b13bb028d9de
ee37737 <geimsac3@gmail.com>
2024-10-04 18:35:21 +0200

    Initial commit

---
+++ src/main/java/net/ordo/modid/ModTutorialDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ordo.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModTutorialDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
